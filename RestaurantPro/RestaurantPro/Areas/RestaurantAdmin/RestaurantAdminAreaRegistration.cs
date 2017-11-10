c3bd7bd9571e6d9e77832c5b774f91c6a2b83931
wanghao852258 <wanghao8522@gmail.com>
2017-11-10 14:55:20 +1300

    v0.1 setup projects

---
+++ RestaurantPro/RestaurantPro/Areas/RestaurantAdmin/RestaurantAdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace RestaurantPro.Areas.RestaurantAdmin
+{
+    public class RestaurantAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "RestaurantAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "RestaurantAdmin_default",
+                "RestaurantAdmin/{controller}/{action}/{id}",
+                new {Controller="SysAdmin", action = "Index", id = UrlParameter.Optional },
+                namespaces:new string[] { "RestaurantPro.Areas.RestaurantAdmin.Controllers" }
+            );
+        }
+    }
+}
