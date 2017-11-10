e2222080614765aa3bb8a976f99b380a64c6c305
wanghao852258 <wanghao8522@gmail.com>
2017-11-10 15:57:16 +1300

    V0.3 test

--- RestaurantPro/TestProject/Program.cs
+++ RestaurantPro/TestProject/Program.cs
@@ -11,6 +11,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Test from Developer b");
+            Console.WriteLine("Test from D A");
         }
     }
 }
