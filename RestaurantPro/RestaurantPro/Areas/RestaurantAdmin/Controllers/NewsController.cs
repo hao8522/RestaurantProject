da787732ecd5ef299372d45c2a18104a784bf876
wanghao852258 <wanghao8522@gmail.com>
2017-11-15 18:04:07 +1300

    Add News

---
+++ RestaurantPro/RestaurantPro/Areas/RestaurantAdmin/Controllers/NewsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+using BLL;
+
+namespace RestaurantPro.Areas.RestaurantAdmin.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: RestaurantAdmin/News
+        [Authorize]
+        public ActionResult LoadNewsForm()
+        {
+            List<NewsCategory> categoryList = new NewsManager().GetAllCategory();
+            SelectList list = new SelectList(categoryList,"CategoryId","CategoryName");
+            ViewBag.dList = list;
+            return View("AddNews");
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult AddNews(News news)
+        {
+            int result = new NewsManager().AddNews(news);
+
+            if (result > 0)
+            {
+
+                return Content("<script>alert('Add News Successfully');location.href='"+Url.Action("LoadNewsForm")+"'</script>");
+
+            }
+            else
+            {
+                return Content("<script>alert('Add News Again');location.href='"+Url.Action("LoadNewsForm")+"'</script>");
+
+            }
+        }
+
+
+    }
+}
