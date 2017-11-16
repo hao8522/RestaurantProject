a4b28e21411207ff2887f61452ca1d511b731c3d
wanghao852 <albert8522@gmail.com>
2017-11-16 19:12:11 +1300

    feedback module
    feedback list, accept feedback

---
+++ RestaurantPro/BLL/FeedbackManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+
+namespace BLL
+{
+    public class FeedbackManager
+    {
+        private FeedbackService feedbackService = new FeedbackService();
+        public int AddFeedback(Suggestion suggestion)
+        {
+            return feedbackService.AddFeedback(suggestion);
+        }
+
+
+        public List<Suggestion> GetSuggestion(){
+
+            return feedbackService.GetSuggestion();
+        }
+
+
+        public int AcceptFeedback(int suggestionId)
+        {
+            return feedbackService.AcceptFeedback(suggestionId);
+        }
+    }
+
+      
+}
---
+++ RestaurantPro/DAL/Service/FeedbackService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using System.Data.Entity;
+
+
+namespace DAL
+{
+    public class FeedbackService
+    {
+        /// <summary>
+        /// add feedback
+        /// </summary>
+        /// <param name="suggestion"></param>
+        /// <returns></returns>
+
+        public int AddFeedback(Suggestion suggestion)
+        {
+
+            using (RestaurantDBEntities db = new RestaurantDBEntities())
+            {
+                db.Suggestions.Add(suggestion);
+                return db.SaveChanges();
+            };
+        }
+
+        /// <summary>
+        /// get feedback
+        /// </summary>
+        /// <returns></returns>
+        public List<Suggestion> GetSuggestion()
+        {
+            using (RestaurantDBEntities db = new RestaurantDBEntities())
+            {
+                return (from s in db.Suggestions where s.StatusId == 0 orderby s.SuggestionTime descending select s).ToList();
+
+            }
+        }
+        /// <summary>
+        /// accept feedback
+        /// </summary>
+        /// <param name="suggestionId"></param>
+        /// <returns></returns>
+        public int AcceptFeedback(int suggestionId)
+        {
+            using(RestaurantDBEntities db= new RestaurantDBEntities()){
+                Suggestion suggestion = (from s in db.Suggestions where s.SuggestionId == suggestionId select s).FirstOrDefault();
+                db.Entry<Suggestion>(suggestion).State = EntityState.Modified;
+                suggestion.StatusId = 1;
+
+                return db.SaveChanges();
+            }
+            
+        }
+    }
+}
