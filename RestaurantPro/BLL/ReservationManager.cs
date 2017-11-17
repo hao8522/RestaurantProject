9c969ffda7fb67ee2117af08124878c61e4b055f
wanghao852 <albert8522@gmail.com>
2017-11-17 14:32:13 +1300

    reservation module
    admin can
    - check reservation,
    -cancel reservation,
    -delete reservation

---
+++ RestaurantPro/BLL/ReservationManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL;
+
+namespace BLL
+{
+   public  class ReservationManager
+    {
+       private ReservationService reservationService = new ReservationService();
+
+
+
+       public int ModifyReservation(int bookId, int statId)
+       {
+
+           return reservationService.ModifyReservation(bookId, statId);
+       }
+
+
+        public List<DishesBook> GetAllReservatoin()
+        {
+
+            return reservationService.GetAllReservatoin();
+        }
+    }
+}
---
+++ RestaurantPro/DAL/Service/ReservationService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL
+{
+    public class ReservationService
+    {
+
+       
+        /// <summary>
+        /// modify reservation
+        /// </summary>
+        /// <param name="bookId"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+        public int ModifyReservation(int bookId, int statId)
+        {
+            using (RestaurantDBEntities db = new RestaurantDBEntities())
+            {
+                DishesBook dishBook= new DishesBook();
+
+                dishBook.BookId = bookId;
+
+                db.DishesBooks.Attach(dishBook);
+
+                dishBook.OrderStatus = Convert.ToInt32(statId);
+                return db.SaveChanges();
+               
+            }
+
+        }
+
+        /// <summary>
+        /// get all reservation
+        /// </summary>
+        /// <returns></returns>
+        public List<DishesBook> GetAllReservatoin()
+        {
+            using (RestaurantDBEntities db = new RestaurantDBEntities())
+            {
+                return (from d in db.DishesBooks where d.OrderStatus==0 || d.OrderStatus==1 orderby d.BookTime descending select d).ToList();
+            }
+        }
+    }
+}
